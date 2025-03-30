b2c5c806ec9a8971be123499343137be9e28ecf6
zed20121 <raufahmad.imtiaz@students.mq.edu.au>
2025-03-30 22:22:47 +1100

    fixed
    fixed loop error

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -56,7 +56,7 @@ public class App {
             String line;
             int i = 1;
             while ((line = br.readLine()) != null) {
-                System.out.println(line);
+               // System.out.println(line);
                 wordleDatabaseConnection.addValidWord(i, line);
                 i++;
             }
@@ -70,20 +70,32 @@ public class App {
         // let's get them to enter a word
 
         try (Scanner scanner = new Scanner(System.in)) {
+            
+
+
             System.out.print("Enter a 4 letter word for a guess or q to quit: ");
             String guess = scanner.nextLine();
 
             while (!guess.equals("q")) {
-                System.out.println("You've guessed '" + guess+"'.");
+                if (guess.matches("^[a-zA-Z]{4}$")) {
+
+                    System.out.println("You've guessed '" + guess+"'.");
 
                 if (wordleDatabaseConnection.isValidWord(guess)) { 
                     System.out.println("Success! It is in the the list.\n");
-                }else{
-                    System.out.println("Sorry. This word is NOT in the the list.\n");
+                } else {
+                    System.out.println("Sorry. This word is NOT in the list.\n");
                 }
 
-                System.out.print("Enter a 4 letter word for a guess or q to quit: " );
-                guess = scanner.nextLine();
+                    System.out.print("Enter a 4 letter word for a guess or q to quit: " );
+                    guess = scanner.nextLine();
+
+                } else {
+                    System.out.print("Enter a 4 letter word for a guess or q to quit: " );
+                    guess = scanner.nextLine();
+                }
+
+                
             }
         } catch (NoSuchElementException | IllegalStateException e) {
             e.printStackTrace();
